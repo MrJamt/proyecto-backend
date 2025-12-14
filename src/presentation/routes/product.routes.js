29ff0c90e48a1c77eb229f7c336a19c6622de0a7
mlaurel19 <mauricio.laurel.19@gmail.com>
2025-12-14 12:31:34 -0400

    ejercicio1 corregido, isadmin

--- src/presentation/routes/product.routes.js
+++ src/presentation/routes/product.routes.js
@@ -16,8 +16,8 @@ const productController = new ProductController(productService);
 const router = Router();
 router.get('/', asyncHandler(productController.getAll));
 router.get('/:id', asyncHandler(productController.getById));
-router.post('/', asyncHandler(productController.create));
-router.put('/:id', asyncHandler(productController.update));
-router.delete('/:id', asyncHandler(productController.delete));
+router.post('/', [authenticateToken, isAdmin], asyncHandler(productController.create));
+router.put('/:id', [authenticateToken, isAdmin], asyncHandler(productController.update));
+router.delete('/:id', [authenticateToken, isAdmin], asyncHandler(productController.delete));
 
 module.exports = router;
